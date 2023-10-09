b247d3176bba5f534734455acf19210c133891fb
alekskahawai <vol17004@byui.edu>
2023-10-09 21:26:31 +0100

    w02

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,14 @@
+// responsive navigation 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
+
+// footer 
+// current year
+document.querySelector('#year').textContent = new Date().getFullYear();
+// last Modified
+document.querySelector('#lastModified').textContent = Date(document.lastModified);

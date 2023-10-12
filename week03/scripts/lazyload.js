835c955ab2d6ed9d0ad68b615af4985842a91938
alekskahawai <vol17004@byui.edu>
2023-10-12 17:42:45 +0100

    w03

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,5 @@
+// footer 
+// current year
+document.querySelector('#year').textContent = new Date().getFullYear();
+// last Modified
+document.querySelector('#lastModified').textContent = Date(document.lastModified);

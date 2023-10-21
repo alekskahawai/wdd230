83365a4577c8d35cdb81756d7e9c29aaea7f8cc8
alekskahawai <vol17004@byui.edu>
2023-10-21 16:39:31 +0100

    w05

--- scripts/links.js
+++ scripts/links.js
@@ -7,25 +7,10 @@ async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
     // console.log(data);
-    displayLinks(data);
+    displayLinks(data.weeks);
 }
 
 const displayLinks = (weeks) => {
-    // loop through the array of weeks
-    weeks.forEach((week) => {
-        const listItem = document.createElement("li");
-        links.appendChild(listItem);
-        listItem.innerHTML = `${week.week}:`;
-
-        // loop through each link section
-        week.links.forEach((link) => {
-            console.log(link.title); console.log(link.url);
-            listItem.innerHTML += ` <a href="${link.url}">${link.title}</a> | `;
-        })
-    })
-}
-
-/* const displayLinks = (weeks) => {
     weeks.forEach((week) => {
         let li = document.createElement('li');
 
@@ -39,6 +24,6 @@ const displayLinks = (weeks) => {
 
 
     })
-} */
+}
 
 getLinks();

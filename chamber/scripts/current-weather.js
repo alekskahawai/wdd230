32ce36c6966dd9fc583bf3013632254afd1a7e5f
alekskahawai <vol17004@byui.edu>
2023-10-26 00:31:07 +0100

    w07

--- chamber/scripts/current-weather.js
+++ chamber/scripts/current-weather.js
@@ -4,24 +4,12 @@ const captionDesc = document.querySelector('#weather-desc');
 const url = 'https://api.openweathermap.org/data/2.5/weather?lat=56.97&lon=24.10&appid=bd5c2931011c54c7ccaf7bdace44278d&units=metric'
 
 async function apiFetch() {
-    try {
-        const response = await fetch(url);
-        if (response.ok) {
-            const data = await response.json();
-            // console.log(data);
-            displayResults(data);
-        }
-        else {
-            throw Error(await response.text())
-        }
-    }
-    catch (error) {
-        console.log(error);
-    }
+    const response = await fetch(url);
+    const data = await response.json();
+    displayResults(data);
 }
 
 function displayResults(data) {
-    // currentTemp.innerHTML = `${data.main.temp.toString().substring(0, 2)}&deg;C`;
     currentTemp.innerHTML = `${data.main.temp | 0}&deg;C`;
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     let desc = data.weather[0].description;
---
+++ chamber/scripts/forecast-weather.js
@@ -0,0 +1,45 @@
+const forecast1 = document.querySelector('#forecast-1');
+const forecast2 = document.querySelector('#forecast-2');
+const forecast3 = document.querySelector('#forecast-3');
+
+const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=56.97&lon=24.10&appid=bd5c2931011c54c7ccaf7bdace44278d&units=metric'
+
+/* async function apiFetch() {
+    const response = await fetch(forecastURL);
+    const data = await response.json();
+    displayResults(data);
+} */
+
+async function apiFetch() {
+    try {
+        const response = await fetch(forecastURL);
+        if (response.ok) {
+            const data = await response.json();
+
+            displayResults(data);
+        }
+        else {
+            throw Error(await response.text())
+        }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+
+    forecast1.innerHTML = `${data.list[0].dt_txt}: ${data.list[0].main.temp | 0}&deg;C`;
+    forecast2.innerHTML = `${data.list[10].dt_txt}: ${data.list[10].main.temp | 0}&deg;C`;
+    forecast3.innerHTML = `${data.list[20].dt_txt}: ${data.list[20].main.temp | 0}&deg;C`;
+}
+/* function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp | 0}&deg;C`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+} */
+
+apiFetch();

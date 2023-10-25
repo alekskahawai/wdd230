2d5caa872c36b7772192e9d1cc253a2402b2eae7
alekskahawai <vol17004@byui.edu>
2023-10-26 00:45:41 +0100

    w07

--- chamber/scripts/forecast-weather.js
+++ chamber/scripts/forecast-weather.js
@@ -4,20 +4,20 @@ const forecast3 = document.querySelector('#forecast-3');
 
 const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=56.97&lon=24.10&appid=bd5c2931011c54c7ccaf7bdace44278d&units=metric'
 
-async function apiFetch() {
+async function forecastApiFetch() {
     const response = await fetch(forecastURL);
     const data = await response.json();
-    displayResults(data);
+    displayForecastResults(data);
     console.log(data);
 }
 
-/* async function apiFetch() {
+/* async function forecastApiFetch() {
     try {
         const response = await fetch(forecastURL);
         if (response.ok) {
             const data = await response.json();
 
-            displayResults(data);
+            displayForecastResults(data);
         }
         else {
             throw Error(await response.text())
@@ -28,13 +28,13 @@ async function apiFetch() {
     }
 } */
 
-function displayResults(data) {
+function displayForecastResults(data) {
 
     forecast1.innerHTML = `${data.list[0].dt_txt}: ${data.list[0].main.temp | 0}&deg;C`;
     forecast2.innerHTML = `${data.list[10].dt_txt}: ${data.list[10].main.temp | 0}&deg;C`;
     forecast3.innerHTML = `${data.list[20].dt_txt}: ${data.list[20].main.temp | 0}&deg;C`;
 }
-/* function displayResults(data) {
+/* function displayForecastResults(data) {
     currentTemp.innerHTML = `${data.main.temp | 0}&deg;C`;
     const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     let desc = data.weather[0].description;
@@ -43,4 +43,4 @@ function displayResults(data) {
     captionDesc.textContent = `${desc}`;
 } */
 
-apiFetch();
+forecastApiFetch();
